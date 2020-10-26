a25a1de8c02eeb2dbac3f253ff1833fc1b62ec7c
jackZhou <zhouzhen01@gridsum.com>
2020-10-26 16:33:18 +0800

    CompletableFuture 出现异常处理策略

--- src/main/java/com/zhou/threadDemo/CompletableFutureDemo/CompletableFutureDemoThree.java
+++ src/main/java/com/zhou/threadDemo/CompletableFutureDemo/CompletableFutureDemoThree.java
@@ -1,5 +1,7 @@
 package com.zhou.threadDemo.CompletableFutureDemo;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -8,6 +10,7 @@ import java.util.concurrent.ExecutionException;
  * @Author JackZhou
  * @Date 2020/5/10  21:45
  **/
+@Slf4j
 public class CompletableFutureDemoThree {
 
     public static void main(String[] args) {
@@ -35,5 +38,29 @@ public class CompletableFutureDemoThree {
         CompletableFuture<String> futureTaskB = CompletableFuture.supplyAsync(() ->  "world");
         CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(futureTaskA, futureTaskB);
         voidCompletableFuture.whenComplete( (t, u) -> System.out.println("任务都完成了 "));
+    }
+
+    /**
+      * @Author JackZhou
+      * @Description  CompletableFuture 出现异常处理策略
+     **/
+    public static void testFutureR(Boolean flag){
+        CompletableFuture.runAsync(() -> {
+            log.info("进入CompletableFuture");
+            if (flag) {
+                throw new RuntimeException("测试异步异常");
+            }
+            log.info("进入CompletableFuture结束");
+        }).whenComplete((r, e) ->{
+            if(e != null){
+                log.error("执行未完成出现异常", e);
+            }
+        }).exceptionally(e -> {
+            log.error("exceptionally出现异常", e);
+            return null;
+        }).handle((t, e) -> {
+            log.error("hander 处理异常", e);
+            return null;
+        }).join();
     }
 }
